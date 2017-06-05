54bdd623cabc1d903f8119f1eba878e8bd47322c
西田直矢 <naoya@nishidanaoyanoMacBook-Pro.local>
2017-06-06 00:38:51 +0900

    Until it was developed by XR jam

---
+++ Assets/ButtonScript.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ButtonScript : MonoBehaviour {
+
+	[SerializeField] private GameObject player;
+	[SerializeField] private GameObject enemy;
+
+	public void ClickTest () {
+		Debug.Log ("Clicked.");
+
+		GameObject buttonA = GameObject.Find ("Canvas/ButtonA");
+		buttonA.SetActive(true);
+
+		//GameObject unitychan = GameObject.Find ("ImageTarget/unitychan");
+		player.SetActive(true);
+		player.transform.position = new Vector3 (0f, 0.01f, 0f);
+		//GameObject enemy = GameObject.Find ("ImageTarget/z@walk");
+		enemy.SetActive(true);
+		enemy.transform.position = new Vector3 (1.15f, 0.01f, 0f);
+
+
+	}
+}
---
+++ Assets/EnemyCollider.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyCollider : MonoBehaviour {
+
+	// 衝突した瞬間に呼ばれる  
+	void OnTriggerEnter(Collider other) {  
+		Debug.Log ("Game Over");
+	}  
+	// 衝突している間呼ばれ続ける  
+	void OnTriggerStay(Collider other) {  
+	}  
+	// 衝突から離れた瞬間に呼ばれる  
+	void OnTriggerExit(Collider other) {  
+	}  
+	void OnCollisionEnter(Collision collision){  
+		foreach(ContactPoint point in collision.contacts){  
+			Vector3 hitPos = point.point;  
+			// hitPosの座標を使ってエフェクトなどを生成  
+		}  
+	} 
+}
